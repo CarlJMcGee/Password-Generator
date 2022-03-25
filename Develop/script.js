e9f0b40c4ee249a9efef794cf29b94d6bbcdbcf0
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-03-25 14:42:51 -0500

    generator creates a random string of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,129 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
+};
+// password potential values
+var passValuesLower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var passValuesUpper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var passValuesSpec = [
+  "!",
+  '"',
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  "\\",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "|",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+// password legnth
+var passLength = parseInt("10");
+
 // Write password to the #password input
 function writePassword() {
+  // debugger;
+  // generate a password
+  var generatePassword = function () {
+    // create array to hold password
+    var letter = [];
+    // loop gen for password length
+    for (i = 0; i <= (2 * passLength) / 6 - 1; i++) {
+      // add a random letter/specChar to array
+      letter.push(passValuesLower[randomNumber(0, passValuesLower.length)]);
+      letter.push(passValuesUpper[randomNumber(0, passValuesUpper.length)]);
+      letter.push(passValuesSpec[randomNumber(0, passValuesSpec.length)]);
+      console.log(letter);
+    }
+    // join password array into string the return string
+    letter = letter.join("");
+    return letter;
+  };
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
